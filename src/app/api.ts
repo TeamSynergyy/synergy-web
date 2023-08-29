be88431a201f9e02632a535842265c869a8c9a50
오승태 <69510981+yeoularu@users.noreply.github.com>
2023-08-30 00:06:49 +0900

    11/feat (#15)
    * feat: 포스트 생성(글 쓰기)
    
    * feat: 포스트 삭제
    
    * fix: 포스트 삭제 모킹 에러
    
    * feat: api response 수정, 프로젝트 생성/상세페이지
    
    * feat: 프로젝트 전체조회, 삭제
    
    * feat: getUsers api 추가
    
    * feat: 유저 프로필 카드 컴포넌트 작성
    
    * feat: 유저프로필 UI, 아바타 클릭시 프로필로 링크, 유저 팔로우/취소 구현
    
    * feat: 최근 포스트/프로젝트 무한스크롤
    
    * feat: 검색 UI 구현
    
    * fix: UserSkeleton 및 검색 세부페이지 오타 수정
    
    * fix: 프로젝트 검색/home nav 오타 수정
    
    * feat: 검색, 프로필 구현
    
    * feat: auth token 로그인시 받아 헤더에 포함

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,29 +1,44 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Post, Project, User, ChatRoom } from "types";
+import { RootState } from "./store";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
+
   tagTypes: [
     "Post",
     "Project",
     "LikedPostId",
     "LikedProjectId",
+    "FollowingId",
+    "FollowerId",
     "ChatRoom",
     "User",
+    "MyInfo",
   ],
   endpoints: (build) => ({
     // Auth
-    register: build.mutation<void, { email: string; password: string }>({
+    register: build.mutation<
+      void,
+      { email: string; password: string; name: string }
+    >({
       query: (credentials) => ({
-        url: "/api/v1/members/join",
+        url: "/api/v1/members/signup",
         method: "POST",
         body: credentials,
       }),
     }),
 
-    login: build.mutation<void, { email: string; password: string }>({
+    login: build.mutation<string, { email: string; password: string }>({
       query: (credentials) => ({
         url: "/api/v1/members/login",
         method: "POST",
@@ -32,8 +47,9 @@ export const api = createApi({
     }),
 
     // MyInfo
-    getMyId: build.query<number, null>({
-      query: () => "/me/id",
+    getMyInfo: build.query<User & { followerIds: number[] }, null>({
+      query: () => "/me/info",
+      providesTags: [{ type: "MyInfo" }],
     }),
 
     getMyLikedPosts: build.query<number[], null>({
@@ -44,6 +60,11 @@ export const api = createApi({
     getMyLikedProjects: build.query<number[], null>({
       query: () => "/me/like/project",
       providesTags: [{ type: "LikedProjectId", id: "LIST" }],
+    }),
+
+    getMyFollowing: build.query<number[], null>({
+      query: () => "/me/following",
+      providesTags: [{ type: "FollowingId", id: "LIST" }],
     }),
 
     getMyChatRooms: build.query<ChatRoom[], null>({
@@ -60,11 +81,24 @@ export const api = createApi({
           : [{ type: "ChatRoom", id: "LIST" }],
     }),
 
+    editMyInfo: build.mutation<void, Partial<User>>({
+      query: (data) => ({
+        url: "/me/info",
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "MyInfo" },
+        { type: "User", id: String(arg.id) },
+      ],
+    }),
+
     likePost: build.mutation<void, number>({
       query: (id) => ({
-        url: `/post/like/${id}`,
+        url: `/post/${id}/like`,
         method: "PUT",
       }),
+
       invalidatesTags: (result, error, arg) => [
         { type: "Post", id: String(arg) },
         { type: "LikedPostId", id: "LIST" },
@@ -73,12 +107,24 @@ export const api = createApi({
 
     likeProject: build.mutation<void, number>({
       query: (id) => ({
-        url: `/project/like/${id}`,
+        url: `/project/${id}/like`,
         method: "PUT",
       }),
+
       invalidatesTags: (result, error, arg) => [
         { type: "Project", id: String(arg) },
         { type: "LikedProjectId", id: "LIST" },
+      ],
+    }),
+
+    follow: build.mutation<void, number>({
+      query: (id) => ({
+        url: `/members/follow/${id}`,
+        method: "PUT",
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "User", id: String(arg) },
+        { type: "FollowingId", id: "LIST" },
       ],
     }),
 
@@ -98,88 +144,151 @@ export const api = createApi({
       providesTags: (result, error, arg) => [{ type: "User", id: String(arg) }],
     }),
 
+    getUsers: build.query<User[], number[]>({
+      query: (ids) => `/members?ids=${ids.join(",")}`,
+      providesTags: (result, error, arg) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({
+                type: "User" as const,
+                id: String(id),
+              })),
+              { type: "User", id: "LIST" },
+            ]
+          : [{ type: "User", id: "LIST" }],
+    }),
+
     // Post
     createPost: build.mutation<void, { title: string; content: string }>({
       query: (post) => ({
-        url: "/post/create",
+        url: "/post",
         method: "POST",
-        body: {
-          subject: post.title,
-          content: post.content,
-        },
+        body: post,
       }),
       invalidatesTags: [{ type: "Post", id: "LIST" }],
     }),
 
-    getAllPosts: build.query<{ data: Post[] }, null>({
-      query: () => "/post/postAll",
-      providesTags: (result, error, arg) =>
-        result
-          ? [
-              ...result.data.map(({ id }) => ({
-                type: "Post" as const,
-                id: String(id),
-              })),
-              { type: "Post", id: "LIST" },
-            ]
-          : [{ type: "Post", id: "LIST" }],
-    }),
-
-    getPost: build.query<{ data: Post }, number>({
+    getPost: build.query<Post, number>({
       query: (id) => `/post/${id}`,
       providesTags: (result, error, arg) => [{ type: "Post", id: String(arg) }],
     }),
 
-    deletePost: build.mutation<void, { id: number }>({
-      query: ({ id }) => ({
-        url: `/post/delete/${id}`,
+    getRecentPosts: build.query<
+      { contents: Post[]; totalPages: number },
+      number
+    >({
+      query: (page) => `/post/recent?page=${page}`,
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      merge: (currentCache, newItems) => {
+        currentCache.contents.push(...newItems.contents);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+    }),
+
+    deletePost: build.mutation<void, number>({
+      query: (id) => ({
+        url: `/post/${id}`,
         method: "DELETE",
       }),
       invalidatesTags: (result, error, arg) => [
-        { type: "Post", id: String(arg.id) },
+        { type: "Post", id: String(arg) },
       ],
     }),
 
     // Project
-    createProject: build.mutation<number, Omit<Project, "id" | "likes">>({
+    createProject: build.mutation<
+      number,
+      Omit<Project, "id" | "likes" | "teamMemberIds">
+    >({
       query: (project) => ({
-        url: "/project/create",
+        url: "/project",
         method: "POST",
         body: project,
       }),
       invalidatesTags: [{ type: "Project", id: "LIST" }],
     }),
 
-    getAllProjects: build.query<{ data: Project[] }, null>({
-      query: () => "/project/projectAll",
-      providesTags: (result, error, arg) =>
+    getProject: build.query<Project, { id: number }>({
+      query: ({ id }) => `/project/${id}`,
+      providesTags: (result) =>
         result
-          ? [
-              ...result.data.map(({ id }) => ({
-                type: "Project" as const,
-                id: String(id),
-              })),
-              { type: "Project", id: "LIST" },
-            ]
+          ? [{ type: "Project", id: String(result.id) }]
           : [{ type: "Project", id: "LIST" }],
     }),
 
-    getProject: build.query<{ data: Project }, { id: number }>({
-      query: ({ id }) => `/project/search/${id}`,
-      providesTags: (result) =>
-        result
-          ? [{ type: "Project", id: String(result.data.id) }]
-          : [{ type: "Project", id: "LIST" }],
+    getRecentProjects: build.query<
+      { contents: Project[]; totalPages: number },
+      number
+    >({
+      query: (page) => `/project/recent?page=${page}`,
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      merge: (currentCache, newItems) => {
+        currentCache.contents.push(...newItems.contents);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
 
     deleteProject: build.mutation<void, { id: number }>({
       query: ({ id }) => ({
-        url: `/project/delete/${id}`,
+        url: `/project/${id}`,
         method: "DELETE",
       }),
       invalidatesTags: (result, error, arg) => [
         { type: "Post", id: String(arg.id) },
       ],
+    }),
+
+    // Search
+    searchPosts: build.query<
+      { contents: Post[]; totalPages: number; totalElements: number },
+      [string, number]
+    >({
+      query: ([keyword, page]) =>
+        `/search/post?keyword=${keyword}&page=${page}`,
+    }),
+
+    searchProjects: build.query<
+      { contents: Project[]; totalPages: number; totalElements: number },
+      [string, number]
+    >({
+      query: ([keyword, page]) =>
+        `/search/project?keyword=${keyword}&page=${page}`,
+    }),
+
+    searchUsers: build.query<
+      { contents: User[]; totalPages: number; totalElements: number },
+      [string, number]
+    >({
+      query: ([keyword, page]) =>
+        `/search/user?keyword=${keyword}&page=${page}`,
+    }),
+
+    getPostsByUser: build.query<
+      { contents: Post[]; totalPages: number },
+      [number, number]
+    >({
+      query: ([userId, page]) => `/user/${userId}/posts?page=${page}`,
+      serializeQueryArgs: ({ queryArgs, endpointName }) => {
+        return endpointName + queryArgs[0];
+      },
+      merge: (currentCache, newItems) => {
+        currentCache.contents.push(...newItems.contents);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+    }),
+
+    getProjectsByUser: build.query<Project[], number>({
+      query: (userId) => `/user/${userId}/projects`,
     }),
   }),
 });
