5312cf42800c00196254cbd4d3fa0b0f4312e23a
yeoularu <yeoularu@gmail.com>
2023-09-07 19:32:42 +0900

    fix

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -122,7 +122,7 @@ export default function PostCard({ post }: { post: Post }) {
       <Card.Section className={classes.section}>
         <Flex w="100%" justify="space-between" align="center">
           <PostLike {...post} />
-          <ActionIcon onClick={() => navigate(`/posts/${post.postId}`)}>
+          <ActionIcon onClick={() => navigate(`/post/${post.postId}`)}>
             <IconMessage size="1.25rem" />
           </ActionIcon>
         </Flex>
