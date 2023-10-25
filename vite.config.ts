6de94e9a5bf3e4d7e2132db8904fe925e65418aa
yeoularu <yeoularu@gmail.com>
2023-10-25 22:56:44 +0900

    Change proxy configuration to support API requests to localhost:8080.

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig({
         target: "https://synergyy.link",
         changeOrigin: true, // host 헤더를 변경합니다.
         secure: false, // SSL 설정 (https 프로토콜)
-
+      },
+      "localhost:8080/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true, // host 헤더를 변경합니다.
+        secure: false, // SSL 설정 (https 프로토콜)
       },
     },
   },
