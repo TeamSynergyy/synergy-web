44997b018c1d8f48655e7871606c50b7d107d2a6
yeoularu <yeoularu@gmail.com>
2023-10-25 23:00:46 +0900

    [proxy] Update target URL for /api route to https://synergyy.link

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,8 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     proxy: {
-      "/api": {
+      "https://synergyy.link/api": {
         target: "https://synergyy.link",
-        changeOrigin: true, // host 헤더를 변경합니다.
-        secure: false, // SSL 설정 (https 프로토콜)
-      },
-      "localhost:8080/api": {
-        target: "http://localhost:8080",
         changeOrigin: true, // host 헤더를 변경합니다.
         secure: false, // SSL 설정 (https 프로토콜)
       },
