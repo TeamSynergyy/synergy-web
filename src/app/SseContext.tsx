f328cd1c91ae8b37b532a5c9449b3f7efb9e54d8
yeoularu <yeoularu@gmail.com>
2023-12-07 20:15:10 +0900

    [SseContext.tsx] Update SseProvider component to include support for handling Last-Event-Id header in SSE connections.

--- src/app/SseContext.tsx
+++ src/app/SseContext.tsx
@@ -1,8 +1,9 @@
-import { createContext, useEffect, useRef } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import {
   EventSourcePolyfill,
   MessageEvent,
   Event,
+  EventSourcePolyfillInit,
 } from "event-source-polyfill";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentToken } from "./authSlice";
@@ -21,10 +22,17 @@ export const SseProvider = ({ children }: { children: JSX.Element }) => {
   const hostUrl = import.meta.env.VITE_API_URL;
   const token = useSelector(selectCurrentToken);
   const esRef = useRef<EventSourcePolyfill | null>(null);
+  const [lastEventId, setLastEventId] = useState<string | null>(null);
 
   const sseHandler = (event: Event) => {
     const messageEvent = event as MessageEvent;
     console.log("SSE message received", messageEvent);
+
+    // Store the last event ID
+    if (messageEvent.lastEventId) {
+      setLastEventId(messageEvent.lastEventId);
+    }
+
     if (messageEvent.data[0] !== "{") return;
     dispatch(sseMessageReceived(messageEvent));
   };
@@ -32,10 +40,16 @@ export const SseProvider = ({ children }: { children: JSX.Element }) => {
   useEffect(() => {
     if (!token) return;
 
+    const headers: { [name: string]: string } = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    if (lastEventId) {
+      headers["Last-Event-Id"] = lastEventId;
+    }
+
     const eventSource = new EventSourcePolyfill(`${hostUrl}/api/v1/subscribe`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      headers: headers,
       heartbeatTimeout: 600000,
     });
 
